8f6fe85463cd9eea36957defc5b2f1a00aa20bed
howkj1 <jhowk@sai-dev-jim.local>
2015-09-25 14:07:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/anon/pen/YypPBM.
+
+ Animated Skills graph using jQuery/HTML/CSS
+
+Forked from [Thomas Wicker](http://codepen.io/thomasjwicker/)'s Pen [Animated HTML/CSS/jQuery Skills Graph](http://codepen.io/thomasjwicker/pen/vHyui/).
---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('.adobe').css('width', '100%');
+  $('.html').css('width', '100%');
+  $('.css').css('width', '100%');
+  $('.lesssass').css('width', '50%');
+  $('.jquery').css('width', '70%');
+  $('.javascript').css('width', '55%');
+  $('.bootstrap').css('width', '90%');
+  $('.wordpress').css('width', '85%');
+  $('.ui').css('width', '100%');
+  $('.ux').css('width', '90%');
+  $('.rwd').css('width', '85%');
+  $('.mobile').css('width', '85%');
+  $('.rapidproto').css('width', '95%');
+});
